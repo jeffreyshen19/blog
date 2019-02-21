60e7f9dcaa67d555c86dc1cdbbad3a75f5035744
jeffreyshen19 <jshen@andover.edu>
2019-02-20 20:36:27 -0500

    fix build error

--- _posts/2019-01-02-welcome-to-jekyll.markdown
+++ _posts/2019-01-02-welcome-to-jekyll.markdown
@@ -1,9 +1,11 @@
 ---
 layout: post
 title:  "Welcome to Jekyll!"
+subtitle: "This is a subtitle."
 date:   2019-01-02 14:43:26 -0800
 categories: jekyll update
 source_code: "[link]"
+featured_image: "test.jpg"
 references: [{title: "Title of Reference", url: "https://example.com"}]
 ---
 You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,39 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minify = require('gulp-minify');
+var child = require('child_process');
+var gutil = require('gulp-util');
 
 gulp.task('sass', function () {
-  return gulp.src('assets/src/SCSS/*.scss')
+  return gulp.src('assets/_src/SCSS/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('assets/dist/CSS'));
 });
 
 gulp.task('compress', function() {
-  return gulp.src('assets/src/JS/*.js')
+  return gulp.src('assets/_src/JS/*.js')
     .pipe(minify({}))
     .pipe(gulp.dest('assets/dist/JS'));
 });
 
-gulp.task('default', gulp.series(gulp.series('sass', 'compress'), function a() {
-  gulp.watch('assets/src/SCSS/*.scss', gulp.series('sass'));
-  gulp.watch('assets/src/JS/*.js', gulp.series('compress'));
+gulp.task('jekyll', function() {
+  var jekyll = child.spawn('jekyll', ['serve',
+    '--watch',
+    '--incremental',
+    '--drafts'
+  ]);
+
+  var jekyllLogger = function(buffer){
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('default', gulp.parallel(gulp.parallel('sass', 'compress', 'jekyll'), function a() {
+  gulp.watch('assets/_src/SCSS/*.scss', gulp.series('sass'));
+  gulp.watch('assets/_src/JS/*.js', gulp.series('compress'));
 }));
