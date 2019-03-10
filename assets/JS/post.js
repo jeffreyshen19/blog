b670427f529b8191329fa87777f5fc3e70495642
jeffreyshen19 <jshen@andover.edu>
2019-03-10 15:59:59 -0700

    refactor

---
+++ assets/JS/post.js
@@ -0,0 +1,9 @@
+---
+---
+
+{% include_relative d3.min.js %}
+{% include_relative charts/Chart.js %}
+{% include_relative charts/line_chart.js %}
+{% include_relative charts/bar_chart.js %}
+{% include_relative initialize.js %}
+{% include_relative resize.js %}
--- assets/javascripts/post.js
+++
@@ -1,9 +0,0 @@
----
----
-
-{% include_relative d3.min.js %}
-{% include_relative Chart.js %}
-{% include_relative line_chart.js %}
-{% include_relative bar_chart.js %}
-{% include_relative initialize.js %}
-{% include_relative resize.js %}
