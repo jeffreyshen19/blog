9f4d2f799db9839619e3adbf01668a480f618f55
jeffreyshen19 <jshen@andover.edu>
2019-05-12 16:23:39 -0400

    update adjacency matrix format

--- _code/88rising/get_artists.py
+++ _code/88rising/get_artists.py
@@ -34,6 +34,12 @@ def getArtists(title):
 
     return list(set(artists))
 
+def getGroup(artist):
+    if artist == "88RISING": return 1
+    elif artist in ["Rich Brian", "Joji", "Keith Ape", "AUGUST 08", "Higher Brothers", "NIKI", "Lexie Liu", "MaSiWei"]: return 2 # current artists
+    elif artist in ["ZHU.", "Yaeji", "Okasian", "Bryan Cha$e", "Phum Viphurit", "HYUKOH (\ud601\uc624)", "KOHH", "Verbal", "Bohan Phoenix", "Jay Park", "HARIKIRI", "Brian Puspos", "Dumbfoundead", "Towkio", "Simon D", "G2", "josh pan", "Suboi", "Rina Sawayama", "Sen Morimoto", "Bengfang", "Dbo", "Tatsuro Yamashita", "Shigeo Sekito", "Jess Connelly", "YURUFUWA GANG", "Japanese Breakfast", "Giraffage", "Eric Nam", "Yeseo", "YENTOWN", "Horim", "CIFIKA", "Gentle Bones", "DMEANOR", "Ikkubaru", "Rainbow Chan", "KID FRESINO", "jjj", "Yllis", "Tanukichan", "Starro", "Guzz"]: return 3 # former artists and asian affiliates
+    else: return 4
+
 with open('video_statistics.csv') as csvfile:
     rows = csv.DictReader(csvfile)
 
@@ -62,8 +68,23 @@ with open('video_statistics.csv') as csvfile:
                 if artist not in adjacency[rowArtists[i]]: adjacency[rowArtists[i]][artist] = 1
                 else: adjacency[rowArtists[i]][artist] += 1
 
+    adjacency_formatted = {
+        "nodes": [],
+        "links": []
+    }
+
+    for key in adjacency:
+        adjacency_formatted["nodes"].append({
+            "id": key,
+            "group": getGroup(key),
+            "count": count[artists.index(key)]
+        })
+
+        for key2,value in adjacency[key].items():
+            if not any(d.get('source', None) == key2 and d.get('target', None) == key for d in adjacency_formatted["links"]): adjacency_formatted["links"].append({"source": key, "target": key2, "value": value})
+
     with open('adjacency.json', 'w') as outfile:
-        json.dump(adjacency, outfile)
+        json.dump(adjacency_formatted, outfile)
 
     # Create bar chart of each artist views
     f = open('artist_statistics.csv', "w+")
