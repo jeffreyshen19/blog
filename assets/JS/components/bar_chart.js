5c9b92e764644a6df1a4aad807ff18c79b1f907e
Jeffrey Shen <jjshen@mit.edu>
2020-01-27 02:33:57 -0500

    add react component skeletons

---
+++ assets/JS/components/bar_chart.js
@@ -0,0 +1,20 @@
+const e = React.createElement;
+
+class BarChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+    return (
+      <p></p>
+    );
+  }
+}
+
+// const domContainer = document.querySelector('#bar_chart');
+// ReactDOM.render(e(BarChart), domContainer);
---
+++ assets/JS/components/line_chart.js
@@ -0,0 +1,20 @@
+const e = React.createElement;
+
+class LineChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+    return (
+      <p></p>
+    );
+  }
+}
+
+// const domContainer = document.querySelector('#bar_chart');
+// ReactDOM.render(e(LineChart), domContainer);
