3387b35f775ea0c0419c0daedbb4dfa6a38ab724
Jeffrey Shen <jjshen@mit.edu>
2020-01-28 20:41:54 -0500

    begin refactoring line_chart into a reusable chart

--- dist/JS/components/chart.js
+++ dist/JS/components/chart.js
@@ -1,1 +1,155 @@
-export default class Chart extends React.Component{constructor(a){super(a),this.state={}}render(){return React.createElement("h1",null,"This is a chart")}}
+export default class Chart extends React.Component{constructor(a){// Load data from csv
+super(a),this.state={data:[],chart:null,width:0,margin:this.props.margin,padding:this.props.padding,height:(a.height||300)-this.props.margin.top-this.props.margin.bottom,offset:0,body_width:0},d3.csv(a.csv).then(b=>{this.setState({data:b.map(function(b){return b[a.xcol]=d3.timeParse("%Y-%m-%d")(b[a.xcol]),a.ycols.split(",").forEach(function(a){b[a]=parseFloat(b[a])}),b})})})}updateDimensions(){//Calculate new width
+let a=(d3.select("body").node().offsetWidth-d3.select("#body").node().offsetWidth)/2,b=d3.select("body").node().offsetWidth,c=d3.select("#body").node().offsetWidth-this.state.margin.left-this.state.margin.right-this.state.padding.left-this.state.padding.right+2*a;this.setState({width:c,offset:a,body_width:b})}componentDidMount(){this.updateDimensions(),window.addEventListener("resize",this.updateDimensions.bind(this));// Add D3 selector to state
+let a=d3.select(this.props.chart);this.setState({chart:a})}renderGraph(){console.log(this.state.data)}// renderGraph(){
+//   let margin = this.state.margin,
+//       padding = this.state.padding,
+//       dataset = this.props,
+//       chart = this.state.chart,
+//       data = this.state.data,
+//       width = this.state.width,
+//       height = this.state.height,
+//       offset = this.state.offset,
+//       body_width = this.state.body_width;
+//
+//   // Set the ranges
+//   var	x = d3.scaleTime().range([0, width]),
+//       y = d3.scaleLinear().range([height, 0]);
+//
+//   // Define axes
+//   var xAxis = d3.axisBottom(x),
+//       yAxis = d3.axisLeft(y);
+//
+//   // Set ticks
+//   if(body_width < 400) xAxis.ticks(d3.timeYear.every(8));
+//   else if(body_width < 800) xAxis.ticks(d3.timeYear.every(4));
+//   else xAxis.ticks(d3.timeYear.every(2));
+//
+//   // Reset canvas
+//   chart.selectAll("*").remove();
+//
+//   // Create canvas
+//   var	svg = chart
+//     .append("svg")
+//       .attr("width", width + margin.left + margin.right)
+//       .attr("height", height + margin.top + margin.bottom)
+//       .style("transform", "translate(-" + offset + "px,0px)")
+//     .append("g")
+//       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+//   // Add tooltip and tooltip line
+//   chart.append("div")
+//     .attr("class", "tooltip hidden");
+//
+//   // Fit Domain
+//   var ymax = d3.max(data, function(d) {
+//     return Math.max(...dataset.ycols.split(",").map(function(ycol){
+//       return d[ycol];
+//     }));
+//   });
+//   var xextent = d3.extent(data, function(d) { return d[dataset.xcol]; });
+//   x.domain(xextent);
+//   y.domain([0, ymax]);
+//
+//   // Render data
+//   let colors = dataset.linecolors.split(",");
+//   dataset.ycols.split(",").forEach(function(ycol, i){
+//     var	valueline = d3.line()
+//       .x(function(d) { return x(d[dataset.xcol]); })
+//       .y(function(d) { return y(d[ycol]); });
+//
+//     svg.append("path")
+//       .attr("class", "line")
+//       .style("stroke", colors[i])
+//       .attr("d", valueline(data));
+//   });
+//
+//   // Add X Axis
+//   svg.append("g")
+//     .attr("class", "x axis")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(xAxis);
+//
+//   // Add the Y Axis
+//   svg.append("g")
+//     .attr("class", "y axis")
+//     .call(yAxis);
+//
+//   // Chart title
+//   chart.insert("p", ":first-child")
+//     .html(dataset.title)
+//     .attr("class", "axis-label title");
+//
+//   // X Axis Label
+//   chart.append("p")
+//     .attr("class", "axis-label")
+//     .style("text-align", "center")
+//     .html(dataset.xlabel);
+//
+//   // Y Axis Label
+//   svg.append("text")
+//     .attr("transform", "rotate(-90)")
+//     .attr("class", "axis-label")
+//     .attr("y", 0 - margin.left)
+//     .attr("x", 0 - (height / 2))
+//     .attr("dy", "1em")
+//     .text(dataset.ylabel);
+//
+//   /*
+//     TOOLTIP
+//   */
+//
+//   var ycols = dataset.ycols.split(","),
+//       labels = dataset.linelabels.split(",");
+//
+//   svg.append("line")
+//     .attr("class", "tooltip-line hidden")
+//     .attr("x1", x(xextent[0]))
+//     .attr("y1", y(0))
+//     .attr("x2", x(xextent[0]))
+//     .attr("y2", y(ymax))
+//     .style("stroke", "black")
+//     .style("stroke-width", "1")
+//     .style("stroke-dasharray", "5,5");
+//
+//   var tooltip = chart.select(".tooltip"),
+//       tooltipLine = chart.select(".tooltip-line");
+//
+//   var bisect = d3.bisector(function(d){ return d[dataset.xcol]; }).right;
+//
+//   let positionTooltip = (mouse, tooltip, x, y) => {
+//     return {
+//       "left": (20 + mouse[0] + tooltip.node().offsetWidth > this.state.width + this.state.margin.left + this.state.margin.right ? mouse[0] - 10 - tooltip.node().offsetWidth - this.state.offset: mouse[0] + 10 - this.state.offset),
+//       "top": y(0) - tooltip.node().offsetHeight + this.state.margin.top + 24,
+//     };
+//   }
+//
+//   chart.select("svg").on("mousemove", function(){
+//     var mouse = d3.mouse(this),
+//         mouseX = x.invert(mouse[0] - margin.left),
+//         index = bisect(data, mouseX),
+//         datum = data[index];
+//
+//     if(datum == null){
+//       tooltip.classed("hidden", true);
+//       tooltipLine.classed("hidden", true);
+//     }
+//     else{
+//       tooltipLine.attr("x1", x(datum[dataset.xcol]))
+//         .attr("x2", x(datum[dataset.xcol]))
+//         .classed("hidden", false);
+//
+//       tooltip.classed("hidden", false)
+//         .html("<strong>" + d3.timeFormat("%b %e, %Y")(datum[dataset.xcol]) + "</strong><br>" + ycols.map(function(d, i){
+//           return "<div class = 'tooltip-label'><div class = 'bubble' style = 'background-color:" + colors[i] + "'></div>" + labels[i] + ": " + datum[d].toFixed(2) + "</div>";
+//         }).join(""))
+//         .style("left", positionTooltip(mouse, tooltip, x, y).left + "px")
+//         .style("top", positionTooltip(mouse, tooltip, x, y).top + "px");
+//     }
+//   }).on("mouseout", function(d){
+//     tooltip.classed("hidden", true);
+//     tooltipLine.classed("hidden", true);
+//   });
+//
+// }
+render(){return this.state.data.length&&this.state.chart&&this.renderGraph(),null}}
--- dist/JS/components/line_chart.js
+++ dist/JS/components/line_chart.js
@@ -1,14 +1,153 @@
-import Chart from"/dist/JS/components/chart.js";const e=React.createElement;class LineChart extends React.Component{constructor(a){super(a);const b={top:5,right:20,bottom:20,left:65};// Load data from csv
-this.state={data:[],chart:null,width:0,height:(a.height||300)-b.top-b.bottom,margin:b,padding:{top:40,right:20,bottom:40,left:20},offset:0,body_width:0},d3.csv(a.csv).then(b=>{this.setState({data:b.map(function(b){return b[a.xcol]=d3.timeParse("%Y-%m-%d")(b[a.xcol]),a.ycols.split(",").forEach(function(a){b[a]=parseFloat(b[a])}),b})})})}updateDimensions(){//Calculate new width
-let a=(d3.select("body").node().offsetWidth-d3.select("#body").node().offsetWidth)/2,b=d3.select("body").node().offsetWidth,c=d3.select("#body").node().offsetWidth-this.state.margin.left-this.state.margin.right-this.state.padding.left-this.state.padding.right+2*a;this.setState({width:c,offset:a,body_width:b})}componentDidMount(){this.updateDimensions(),window.addEventListener("resize",this.updateDimensions.bind(this));// Add D3 selector to state
-let a=d3.select(this.props.chart);this.setState({chart:a})}renderGraph(){let a=this.state.margin,b=this.state.padding,c=this.props,d=this.state.chart,e=this.state.data,f=this.state.width,g=this.state.height,h=this.state.offset,i=this.state.body_width;// Set the ranges
-var j=d3.scaleTime().range([0,f]),k=d3.scaleLinear().range([g,0]),l=d3.axisBottom(j),m=d3.axisLeft(k);// Define axes
-400>i?l.ticks(d3.timeYear.every(8)):800>i?l.ticks(d3.timeYear.every(4)):l.ticks(d3.timeYear.every(2)),d.selectAll("*").remove();// Create canvas
-var n=d.append("svg").attr("width",f+a.left+a.right).attr("height",g+a.top+a.bottom).style("transform","translate(-"+h+"px,0px)").append("g").attr("transform","translate("+a.left+","+a.top+")");// Add tooltip and tooltip line
-d.append("div").attr("class","tooltip hidden");// Fit Domain
-var o=d3.max(e,function(a){return Math.max(...c.ycols.split(",").map(function(b){return a[b]}))}),p=d3.extent(e,function(a){return a[c.xcol]});j.domain(p),k.domain([0,o]);// Render data
-let q=c.linecolors.split(",");c.ycols.split(",").forEach(function(a,b){var d=d3.line().x(function(a){return j(a[c.xcol])}).y(function(b){return k(b[a])});n.append("path").attr("class","line").style("stroke",q[b]).attr("d",d(e))}),n.append("g").attr("class","x axis").attr("transform","translate(0,"+g+")").call(l),n.append("g").attr("class","y axis").call(m),d.insert("p",":first-child").html(c.title).attr("class","axis-label title"),d.append("p").attr("class","axis-label").style("text-align","center").html(c.xlabel),n.append("text").attr("transform","rotate(-90)").attr("class","axis-label").attr("y",0-a.left).attr("x",0-g/2).attr("dy","1em").text(c.ylabel);/*
-      TOOLTIP
-    */var r=c.ycols.split(","),s=c.linelabels.split(",");n.append("line").attr("class","tooltip-line hidden").attr("x1",j(p[0])).attr("y1",k(0)).attr("x2",j(p[0])).attr("y2",k(o)).style("stroke","black").style("stroke-width","1").style("stroke-dasharray","5,5");var t=d.select(".tooltip"),u=d.select(".tooltip-line"),v=d3.bisector(function(a){return a[c.xcol]}).right;let w=(a,b,c,d)=>({left:20+a[0]+b.node().offsetWidth>this.state.width+this.state.margin.left+this.state.margin.right?a[0]-10-b.node().offsetWidth-this.state.offset:a[0]+10-this.state.offset,top:d(0)-b.node().offsetHeight+this.state.margin.top+24});d.select("svg").on("mousemove",function(){var b=d3.mouse(this),d=j.invert(b[0]-a.left),f=v(e,d),g=e[f];null==g?(t.classed("hidden",!0),u.classed("hidden",!0)):(u.attr("x1",j(g[c.xcol])).attr("x2",j(g[c.xcol])).classed("hidden",!1),t.classed("hidden",!1).html("<strong>"+d3.timeFormat("%b %e, %Y")(g[c.xcol])+"</strong><br>"+r.map(function(a,b){return"<div class = 'tooltip-label'><div class = 'bubble' style = 'background-color:"+q[b]+"'></div>"+s[b]+": "+g[a].toFixed(2)+"</div>"}).join("")).style("left",w(b,t,j,k).left+"px").style("top",w(b,t,j,k).top+"px"))}).on("mouseout",function(){t.classed("hidden",!0),u.classed("hidden",!0)})}render(){// if(this.state.data.length && this.state.chart) this.renderGraph();
-return React.createElement(Chart,null)}}// Render all line charts
+function _extends(){return _extends=Object.assign||function(a){for(var b,c=1;c<arguments.length;c++)for(var d in b=arguments[c],b)Object.prototype.hasOwnProperty.call(b,d)&&(a[d]=b[d]);return a},_extends.apply(this,arguments)}import Chart from"/dist/JS/components/chart.js";const e=React.createElement;class LineChart extends React.Component{constructor(a){super(a)}// renderGraph(){
+//   let margin = this.state.margin,
+//       padding = this.state.padding,
+//       dataset = this.props,
+//       chart = this.state.chart,
+//       data = this.state.data,
+//       width = this.state.width,
+//       height = this.state.height,
+//       offset = this.state.offset,
+//       body_width = this.state.body_width;
+//
+//   // Set the ranges
+//   var	x = d3.scaleTime().range([0, width]),
+//       y = d3.scaleLinear().range([height, 0]);
+//
+//   // Define axes
+//   var xAxis = d3.axisBottom(x),
+//       yAxis = d3.axisLeft(y);
+//
+//   // Set ticks
+//   if(body_width < 400) xAxis.ticks(d3.timeYear.every(8));
+//   else if(body_width < 800) xAxis.ticks(d3.timeYear.every(4));
+//   else xAxis.ticks(d3.timeYear.every(2));
+//
+//   // Reset canvas
+//   chart.selectAll("*").remove();
+//
+//   // Create canvas
+//   var	svg = chart
+//     .append("svg")
+//       .attr("width", width + margin.left + margin.right)
+//       .attr("height", height + margin.top + margin.bottom)
+//       .style("transform", "translate(-" + offset + "px,0px)")
+//     .append("g")
+//       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+//   // Add tooltip and tooltip line
+//   chart.append("div")
+//     .attr("class", "tooltip hidden");
+//
+//   // Fit Domain
+//   var ymax = d3.max(data, function(d) {
+//     return Math.max(...dataset.ycols.split(",").map(function(ycol){
+//       return d[ycol];
+//     }));
+//   });
+//   var xextent = d3.extent(data, function(d) { return d[dataset.xcol]; });
+//   x.domain(xextent);
+//   y.domain([0, ymax]);
+//
+//   // Render data
+//   let colors = dataset.linecolors.split(",");
+//   dataset.ycols.split(",").forEach(function(ycol, i){
+//     var	valueline = d3.line()
+//       .x(function(d) { return x(d[dataset.xcol]); })
+//       .y(function(d) { return y(d[ycol]); });
+//
+//     svg.append("path")
+//       .attr("class", "line")
+//       .style("stroke", colors[i])
+//       .attr("d", valueline(data));
+//   });
+//
+//   // Add X Axis
+//   svg.append("g")
+//     .attr("class", "x axis")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(xAxis);
+//
+//   // Add the Y Axis
+//   svg.append("g")
+//     .attr("class", "y axis")
+//     .call(yAxis);
+//
+//   // Chart title
+//   chart.insert("p", ":first-child")
+//     .html(dataset.title)
+//     .attr("class", "axis-label title");
+//
+//   // X Axis Label
+//   chart.append("p")
+//     .attr("class", "axis-label")
+//     .style("text-align", "center")
+//     .html(dataset.xlabel);
+//
+//   // Y Axis Label
+//   svg.append("text")
+//     .attr("transform", "rotate(-90)")
+//     .attr("class", "axis-label")
+//     .attr("y", 0 - margin.left)
+//     .attr("x", 0 - (height / 2))
+//     .attr("dy", "1em")
+//     .text(dataset.ylabel);
+//
+//   /*
+//     TOOLTIP
+//   */
+//
+//   var ycols = dataset.ycols.split(","),
+//       labels = dataset.linelabels.split(",");
+//
+//   svg.append("line")
+//     .attr("class", "tooltip-line hidden")
+//     .attr("x1", x(xextent[0]))
+//     .attr("y1", y(0))
+//     .attr("x2", x(xextent[0]))
+//     .attr("y2", y(ymax))
+//     .style("stroke", "black")
+//     .style("stroke-width", "1")
+//     .style("stroke-dasharray", "5,5");
+//
+//   var tooltip = chart.select(".tooltip"),
+//       tooltipLine = chart.select(".tooltip-line");
+//
+//   var bisect = d3.bisector(function(d){ return d[dataset.xcol]; }).right;
+//
+//   let positionTooltip = (mouse, tooltip, x, y) => {
+//     return {
+//       "left": (20 + mouse[0] + tooltip.node().offsetWidth > this.state.width + this.state.margin.left + this.state.margin.right ? mouse[0] - 10 - tooltip.node().offsetWidth - this.state.offset: mouse[0] + 10 - this.state.offset),
+//       "top": y(0) - tooltip.node().offsetHeight + this.state.margin.top + 24,
+//     };
+//   }
+//
+//   chart.select("svg").on("mousemove", function(){
+//     var mouse = d3.mouse(this),
+//         mouseX = x.invert(mouse[0] - margin.left),
+//         index = bisect(data, mouseX),
+//         datum = data[index];
+//
+//     if(datum == null){
+//       tooltip.classed("hidden", true);
+//       tooltipLine.classed("hidden", true);
+//     }
+//     else{
+//       tooltipLine.attr("x1", x(datum[dataset.xcol]))
+//         .attr("x2", x(datum[dataset.xcol]))
+//         .classed("hidden", false);
+//
+//       tooltip.classed("hidden", false)
+//         .html("<strong>" + d3.timeFormat("%b %e, %Y")(datum[dataset.xcol]) + "</strong><br>" + ycols.map(function(d, i){
+//           return "<div class = 'tooltip-label'><div class = 'bubble' style = 'background-color:" + colors[i] + "'></div>" + labels[i] + ": " + datum[d].toFixed(2) + "</div>";
+//         }).join(""))
+//         .style("left", positionTooltip(mouse, tooltip, x, y).left + "px")
+//         .style("top", positionTooltip(mouse, tooltip, x, y).top + "px");
+//     }
+//   }).on("mouseout", function(d){
+//     tooltip.classed("hidden", true);
+//     tooltipLine.classed("hidden", true);
+//   });
+//
+// }
+render(){return React.createElement(Chart,_extends({},this.props,{margin:{top:5,right:20,bottom:20,left:65},padding:{top:40,right:20,bottom:40,left:20}}))}}// Render all line charts
 let elements=document.getElementsByClassName("line-chart");for(let a=0;a<elements.length;a++)ReactDOM.render(e(LineChart,{...elements[a].dataset,chart:elements[a]}),elements[a]);
