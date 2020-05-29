436106ed22fa85da5e9eccd933dde7059691dd17
Jeffrey Shen <jjshen@mit.edu>
2020-05-29 15:27:54 -0700

    join data to DOM

--- dist/JS/posts/chinese-dialects/chinese-dialects.js
+++ dist/JS/posts/chinese-dialects/chinese-dialects.js
@@ -1,1 +1,4 @@
-d3.xml("/data/chinese-dialects/usa_counties.svg").then(a=>{d3.select("#map").node().append(a.documentElement)});
+Promise.all([d3.xml("/data/chinese-dialects/usa_counties.svg"),d3.csv("/data/chinese-dialects/languages.csv")]).then(a=>{d3.select("#map").node().append(a[0].documentElement);let b=d3.select("svg"),c={},d=[];// Join data
+// -- First, get proper order of data
+a[1].forEach(function(a){c[a.County]=a}),b.select("#stylegroup").selectAll("path").each(function(){let a=d3.select(this).select("title").text();a in c?d.push(c[a]):d.push(null)});// -- Then, bind data to DOM
+b.select("#stylegroup").selectAll("path").data(d)});
--- src/JS/posts/chinese-dialects/chinese-dialects.js
+++ src/JS/posts/chinese-dialects/chinese-dialects.js
@@ -1,153 +1,28 @@
-d3.xml('/data/chinese-dialects/usa_counties.svg')
-  .then((data) => {
-      d3.select('#map').node().append(data.documentElement)
+Promise.all([
+  d3.xml('/data/chinese-dialects/usa_counties.svg'),
+  d3.csv('/data/chinese-dialects/languages.csv')
+]).then((data) => {
+  // Add svg
+  d3.select('#map').node().append(data[0].documentElement);
+  let svg = d3.select("svg");
+
+  // Join data
+
+  // -- First, get proper order of data
+  let countyDataDict = {}, countyData = [];
+  data[1].forEach(function(d){
+    countyDataDict[d["County"]] = d;
   })
 
-// var width = 960;
-// var height = 500;
-//
-// // D3 Projection
-// var projection = d3.geo.albersUsa()
-// 				   .translate([width/2, height/2])    // translate to center of screen
-// 				   .scale([1000]);          // scale things down so see entire US
-//
-// // Define path generator
-// var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
-// 		  	 .projection(projection);  // tell path generator to use albersUsa projection
-//
-//
-// // Define linear scale for output
-// var color = d3.scale.linear()
-// 			  .range(["rgb(213,222,217)","rgb(69,173,168)","rgb(84,36,55)","rgb(217,91,67)"]);
-//
-// var legendText = ["Cities Lived", "States Lived", "States Visited", "Nada"];
-//
-// //Create SVG element and append map to the SVG
-// var svg = d3.select("body")
-// 			.append("svg")
-// 			.attr("width", width)
-// 			.attr("height", height);
-//
-// // Append Div for tooltip to SVG
-// var div = d3.select("body")
-// 		    .append("div")
-//     		.attr("class", "tooltip")
-//     		.style("opacity", 0);
-//
-// // Load in my states data!
-// d3.csv("stateslived.csv", function(data) {
-// color.domain([0,1,2,3]); // setting the range of the input data
-//
-// // Load GeoJSON data and merge with states data
-// d3.json("us-states.json", function(json) {
-//
-// // Loop through each state data value in the .csv file
-// for (var i = 0; i < data.length; i++) {
-//
-// 	// Grab State Name
-// 	var dataState = data[i].state;
-//
-// 	// Grab data value
-// 	var dataValue = data[i].visited;
-//
-// 	// Find the corresponding state inside the GeoJSON
-// 	for (var j = 0; j < json.features.length; j++)  {
-// 		var jsonState = json.features[j].properties.name;
-//
-// 		if (dataState == jsonState) {
-//
-// 		// Copy the data value into the JSON
-// 		json.features[j].properties.visited = dataValue;
-//
-// 		// Stop looking through the JSON
-// 		break;
-// 		}
-// 	}
-// }
-//
-// // Bind the data to the SVG and create one path per GeoJSON feature
-// svg.selectAll("path")
-// 	.data(json.features)
-// 	.enter()
-// 	.append("path")
-// 	.attr("d", path)
-// 	.style("stroke", "#fff")
-// 	.style("stroke-width", "1")
-// 	.style("fill", function(d) {
-//
-// 	// Get data value
-// 	var value = d.properties.visited;
-//
-// 	if (value) {
-// 	//If value exists…
-// 	return color(value);
-// 	} else {
-// 	//If value is undefined…
-// 	return "rgb(213,222,217)";
-// 	}
-// });
-//
-//
-// // Map the cities I have lived in!
-// d3.csv("cities-lived.csv", function(data) {
-//
-// svg.selectAll("circle")
-// 	.data(data)
-// 	.enter()
-// 	.append("circle")
-// 	.attr("cx", function(d) {
-// 		return projection([d.lon, d.lat])[0];
-// 	})
-// 	.attr("cy", function(d) {
-// 		return projection([d.lon, d.lat])[1];
-// 	})
-// 	.attr("r", function(d) {
-// 		return Math.sqrt(d.years) * 4;
-// 	})
-// 		.style("fill", "rgb(217,91,67)")
-// 		.style("opacity", 0.85)
-//
-// 	// Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks"
-// 	// http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
-// 	.on("mouseover", function(d) {
-//     	div.transition()
-//       	   .duration(200)
-//            .style("opacity", .9);
-//            div.text(d.place)
-//            .style("left", (d3.event.pageX) + "px")
-//            .style("top", (d3.event.pageY - 28) + "px");
-// 	})
-//
-//     // fade out tooltip on mouse out
-//     .on("mouseout", function(d) {
-//         div.transition()
-//            .duration(500)
-//            .style("opacity", 0);
-//     });
-// });
-//
-// // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-// var legend = d3.select("body").append("svg")
-//       			.attr("class", "legend")
-//      			.attr("width", 140)
-//     			.attr("height", 200)
-//    				.selectAll("g")
-//    				.data(color.domain().slice().reverse())
-//    				.enter()
-//    				.append("g")
-//      			.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-//
-//   	legend.append("rect")
-//    		  .attr("width", 18)
-//    		  .attr("height", 18)
-//    		  .style("fill", color);
-//
-//   	legend.append("text")
-//   		  .data(legendText)
-//       	  .attr("x", 24)
-//       	  .attr("y", 9)
-//       	  .attr("dy", ".35em")
-//       	  .text(function(d) { return d; });
-// 	});
-//
-// });
+  svg.select("#stylegroup").selectAll("path")
+    .each(function(d) {
+      let name = d3.select(this).select("title").text();
+      if(name in countyDataDict) countyData.push(countyDataDict[name])
+      else countyData.push(null)
+    });
+
+  // -- Then, bind data to DOM
+  let counties = svg.select("#stylegroup").selectAll("path")
+    .data(countyData)
+
+});
