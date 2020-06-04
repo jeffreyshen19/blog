3e505708b1a67926b8aeb3b6a7298a56ddfa67f4
Jeffrey Shen <jjshen@mit.edu>
2020-06-03 21:31:19 -0700

    add table

--- dist/JS/posts/lapd-unions/lapd-unions.js
+++ dist/JS/posts/lapd-unions/lapd-unions.js
@@ -1,6 +1,6 @@
-d3.csv("/data/lapd-unions/contributions.csv").then(function(a){d3.select("#reports").select("tbody").selectAll("tr").data(a).enter().append("tr").html(a=>`
-          <td>${a["Date Report Filed"]}</td>
-          <td>${a["Candidate First Name"]} ${a["Candidate Last Name"]}, ${a.Office}</td>
-          <td>${d3.format(",")(parseInt(a.Amount))}</td>
-        `);// Add totals
-let b=a.reduce(function(b,a){return b+parseInt(a.Amount)},0);d3.select("#total").text(`$${d3.format(",")(b)}`)});
+let reports;d3.csv("/data/lapd-unions/contributions.csv").then(function(a){reports=a,updateTable(),d3.select("#dropdown").on("change",updateTable)});function updateTable(){let a=d3.select("#dropdown").node().value,b=reports;"all"!=a&&(b=reports.filter(function(b){return b["Candidate First Name"]+" "+b["Candidate Last Name"]==a})),d3.select("#reports").select("tbody").selectAll("tr").remove(),d3.select("#reports").select("tbody").selectAll("tr").data(b).enter().append("tr").html(a=>`
+        <td>${a["Date Report Filed"]}</td>
+        <td>${a["Candidate First Name"]} ${a["Candidate Last Name"]}, ${a.Office}</td>
+        <td>${d3.format(",")(parseInt(a.Amount))}</td>
+      `);// Add totals
+let c=b.reduce(function(b,a){return b+parseInt(a.Amount)},0);d3.select("#total").text(`$${d3.format(",")(c)}`)}
--- src/JS/posts/lapd-unions/lapd-unions.js
+++ src/JS/posts/lapd-unions/lapd-unions.js
@@ -1,19 +1,36 @@
+let reports;
+
 d3.csv("/data/lapd-unions/contributions.csv")
   .then(function(data){
-    // Add table data
-    d3.select("#reports").select("tbody").selectAll("tr")
-      .data(data)
-      .enter()
-      .append("tr")
-        .html((d) => `
-          <td>${d["Date Report Filed"]}</td>
-          <td>${d["Candidate First Name"]} ${d["Candidate Last Name"]}, ${d["Office"]}</td>
-          <td>${d3.format(",")(parseInt(d["Amount"]))}</td>
-        `);
+    reports = data;
+    updateTable();
 
-    // Add totals
-    let total = data.reduce(function(a, c){
-      return a + parseInt(c["Amount"])
-    }, 0);
-    d3.select("#total").text(`$${d3.format(",")(total)}`)
+    d3.select("#dropdown").on("change", updateTable);
   });
+
+function updateTable(){
+  let filter = d3.select("#dropdown").node().value,
+      data = reports;
+
+  if(filter != "all") data = reports.filter(function(d){
+    return d["Candidate First Name"] + " " + d["Candidate Last Name"] == filter;
+  });
+
+  // Add table data
+  d3.select("#reports").select("tbody").selectAll("tr").remove();
+  d3.select("#reports").select("tbody").selectAll("tr")
+    .data(data)
+    .enter()
+    .append("tr")
+      .html((d) => `
+        <td>${d["Date Report Filed"]}</td>
+        <td>${d["Candidate First Name"]} ${d["Candidate Last Name"]}, ${d["Office"]}</td>
+        <td>${d3.format(",")(parseInt(d["Amount"]))}</td>
+      `);
+
+  // Add totals
+  let total = data.reduce(function(a, c){
+    return a + parseInt(c["Amount"])
+  }, 0);
+  d3.select("#total").text(`$${d3.format(",")(total)}`)
+}
